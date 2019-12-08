64ff93a70b1691f91391c7501d158c68179bc6bc
JeongHyeon Lee <sjeonghyeonz@gmail.com>
2019-12-08 17:54:55 +0900

    commit 3

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world:)");
         System.out.println("I'm Alice, project maintainer");
+        System.out.println("This is my last commit!");
     }
 }
